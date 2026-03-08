32768e80a3b7cb6b8a4eefb71ba13e2a1940a12b
Mayank Gurjar <mayank@Mayanks-MacBook-Air.local>
2026-03-08 18:04:49 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+
+const x=()=>{
+  return (
+    <div>
+  <a href="https://www.google.com/">
+    <Button >Click me</Button>
+  </a>
+</div>
+
+  );
+};
+export default x;
