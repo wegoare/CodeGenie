ecdd16eca15811eacdad64061f00ee525dca1110
Mayank Gurjar <mayank@Mayanks-MacBook-Air.local>
2026-03-13 20:29:54 +0530

    06-error-logs

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "my-app" ,
+    middleware: [sentryMiddleware()],
+});
