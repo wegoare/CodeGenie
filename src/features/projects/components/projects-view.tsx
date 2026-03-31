7765cb5244615ae9130a5f6439e4c0ae9f3f4ea1
Mayank Gurjar <mayank@Mayanks-MacBook-Air.local>
2026-03-31 13:01:39 +0530

    png issue

--- src/features/projects/components/projects-view.tsx
+++ src/features/projects/components/projects-view.tsx
@@ -65,7 +65,7 @@ export const ProjectsView = () => {
           <div className="flex justify-between gap-4 w-full items-center">
 
             <div className="flex items-center gap-2 w-full group/logo">
-              <img src="/logo.svg" alt="Codegenie" className="size-[32px] md:size-[46px]" />
+              <img src="/genie-logo.jpg" alt="Codegenie" className="size-[32px] md:size-[46px]" />
               <h1 className={cn(
                 "text-4xl md:text-5xl font-semibold",
                 font.className,
